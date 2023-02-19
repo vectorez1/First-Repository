73068f1d08ffcf080df67ad7933c1a1e07a6fb0f
Victor Gabriel del Rosario <victordelrosariogalva@gmail.com>
2023-02-18 23:35:50 -0400

    More Organized

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 let title = document.getElementById("main-title");
 let button = document.getElementById("button");
-button.addEventListener(onclick , ()=>{alert("klk")})
+button.addEventListener("click", ()=>{title.innerText = "You Pressed"})
+let container = document.getElementsByClassName('container');
+for(i=0; i < container[0].children.length; i++){
+    container[0].children[i].style.margin = '10px'
+}
